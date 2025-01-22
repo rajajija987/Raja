4bbb48d1e86bf884978778b8ecfbadcfa4dee17d
rajajija987 <ww6715014.@gmail.com>
2025-01-23 02:36:27 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7157470945:AAEjidAU7zP77GgO3gstnm4kB2Me2BR6V8g")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@rajaraj_04")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7855020275")
+
+
+
+
+
+  
